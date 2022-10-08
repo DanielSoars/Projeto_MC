75ac063f66e145302af31f6b4cb552331d1bf99a
DanielSoars <70660349+DanielSoars@users.noreply.github.com>
2022-10-08 19:35:57 -0300

    Update KeyboardControl.py

--- Moviments_Drone/KeyboardControl.py
+++ Moviments_Drone/KeyboardControl.py
@@ -2,7 +2,7 @@ import pygame
 
 def iniciar():
     pygame.init()
-    janela = pygame.display.set_mode(400, 400)
+    janela = pygame.display.set_mode((400, 400))
 
 if __name__ == '__main__':
     iniciar()
